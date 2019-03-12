024af2efca21e9a982b32b2de40cf27785daec69
DubmasterxD <dubmaster@tlen.pl>
2019-03-12 01:31:42 +0100

    117/117 (implemented about up to 90)

---
+++ Udemy RPG Course/Assets/Scripts/BattleChar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleChar : MonoBehaviour
+{
+    [SerializeField] bool isPlayer = false;
+    [SerializeField] string[] movesAvailable = null;
+
+    [SerializeField] string charName = "";
+    [SerializeField] int currentHP = 100;
+    [SerializeField] int maxHP = 100;
+    [SerializeField] int currentMP = 30;
+    [SerializeField] int MaxMP = 30;
+    [SerializeField] int strength = 10;
+    [SerializeField] int defence = 10;
+    [SerializeField] int wpnPower = 10;
+    [SerializeField] int armorPower = 10;
+    [SerializeField] bool hasDied = false;
+
+}
---
+++ Udemy RPG Course/Assets/Scripts/BattleManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    public static BattleManager instance;
+
+    [SerializeField] bool battleActive = false;
+    [SerializeField] GameObject battleSceme = null;
+    [SerializeField] Transform[] playerPositions = null;
+    [SerializeField] Transform[] enemyPositions = null;
+    [SerializeField] BattleChar[] playerPrefabs = null;
+    [SerializeField] BattleChar[] enemyPrefabs = null;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void BattleStart(string[] enemiesToSpawn)
+    {
+        if(!battleActive)
+        {
+            battleActive = true;
+            PlayerController.instance.canMove = false;
+            battleSceme.SetActive(true);
+        }
+    }
+}
